e525728f86352760ebd9571b25f71eb24947ffc1
Carlos Cerda <143562904+futuroinge@users.noreply.github.com>
2023-10-18 16:45:42 -0700

    Add files via upload

---
+++ EVA2_11_CADENAS/src/eva2_11_cadenas/EVA2_11_CADENAS.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_11_cadenas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_11_CADENAS {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cade= "Hola";
+        
+        if(cade.equals("Hola"))
+            System.out.println("Las cadenas son iguales");
+        else
+            System.out.println("Las cadenas son diferentes");
+        //para comparar cadenas es mejor usar equals
+    }
+    
+}
