78ca628aa4d7e6a70a8afd04828d4a2454cc98ee
Carlos Cerda <143562904+futuroinge@users.noreply.github.com>
2023-10-30 17:48:49 -0600

    Add files via upload

---
+++ EVA2_19_WHILE_RETIROS/src/eva2_19_while_retiros/EVA2_19_WHILE_RETIROS.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_19_while_retiros;
+
+import java.util.Scanner;
+
+/*@author invitado
+ */
+public class EVA2_19_WHILE_RETIROS {
+    public static void main(String[] args) {
+        int canti = 1000;
+        Scanner input = new Scanner(System.in);
+        
+        while(canti>0){
+            System.out.println("¿Cuanto desas retirar?");
+            int retiro = input.nextInt();
+            canti -= retiro; //igual a canti= canti-retiro
+            System.out.println("Te quedan $" + canti + " de saldo");
+        }
+    }
+}
